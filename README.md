3092cd68517a21eb4851980dc1930817c129fd2f
Serge Vartanov <vartanov.s@gmail.com>
2018-08-31 22:36:27 -0700

    To-Do List HTML, CSS and JS files complete, edit functionality added

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # To Do List Project
-A responsive To Do List web application built to keep track of a single list of tasks
+A responsive To-Do List web application built to keep track of a single list of tasks
 This project was written in JavaScript with jQuery, HTML and CSS
 
 ## How To Use
@@ -12,4 +12,9 @@ This project was written in JavaScript with jQuery, HTML and CSS
 * To __permanently delete__ the to-do item, click the **trash icon**
 
 ## About This Project
-This project is an expansion of the "ToDo List" project that is part of the [Colt Steele](https://github.com/Colt) ["Web Developer Bootcamp"](https://www.udemy.com/the-web-developer-bootcamp/) on [Udemy](https://www.udemy.com/).
+This project is an expansion of the "To-Do List" project that is part of the [Colt Steele](https://github.com/Colt) ["Web Developer Bootcamp"](https://www.udemy.com/the-web-developer-bootcamp/) on [Udemy](https://www.udemy.com/). Feature expansions beyond the standard project include:
+* Added 'Edit Icon' and functionality. Edit icon appears on mouse-over of to-do item.
+* Clicking edit icon allows for inline editing of any to-do item.
+* Set maximum character length to 24 to avoid overflow on mobile.
+* Edit mode disengaged when user hits enter key or edit icon.
+* Edit mode disengaged if user hits maximum character limit.
--- assets/js/todo-list.js
+++ assets/js/todo-list.js
@@ -1,13 +1,53 @@
-// Check off specific To-Do by clicking on it
-$('ul').on('click', 'li', function complete() {
-  $(this).toggleClass('complete');
+// Show or hide To-Do Input Bar by clicking on '+' icon
+$('.fa-plus').click(() => {
+  $('input[type="text"]').fadeToggle();
 });
 
-// Delete a To-Do by clicking on the inner span
-$('ul').on('click', 'span', function fade(event) {
+// Check off specific To-Do by clicking on it
+const strikeOn = function activateLiStrikeThru() {
+  $('ul').on('click', 'li', function complete() {
+    $(this).toggleClass('complete');
+  });
+};
+
+// Delete a To-Do by clicking on the remove icon
+$('ul').on('click', '.remove', function fade(event) {
   $(this).parent().fadeOut(500, function remove() {
     $(this).remove();
   });
+  strikeOn();
+  event.stopPropagation();
+});
+
+// Edit a To-Do by clicking on the edit icon
+$('ul').on('click', '.edit', function edit(event) {
+  // Get parent <li> and grandpaent <ul> with holder variables
+  const thisLi = $(this).parent();
+  const thisUl = thisLi.parent();
+
+  // Check if to-do is currently editable
+  const todoText = thisLi.children('.todo');
+  if (todoText.attr('contenteditable') === 'false') {
+    // If not editable, make to-do text editable and bring into focus
+    todoText.attr('contenteditable', 'true');
+    todoText.focus();
+    // Deactivate strikethru listener
+    thisUl.off('click', 'li');
+    // Activate listener for 'enter' key and character limit on to-do entry
+    todoText.keypress((e) => {
+      if (e.which === 13 || todoText.text().length > 24) {
+        // Make to-do text no longer editable
+        todoText.attr('contenteditable', 'false');
+        // Reactivate strikethru listener
+        strikeOn();
+      }
+    });
+  } else {
+    // If already editable, make to-do text no longer editable
+    todoText.attr('contenteditable', 'false');
+    // Reactive strikethru listener
+    strikeOn();
+  }
   event.stopPropagation();
 });
 
@@ -17,7 +57,7 @@ $('input[type="text"]').keypress(function submit(event) {
     // Grab user input new To-Do text, place into holder variable
     const toDoText = $(this).val();
     // Create a new li and append to ul
-    $('ul').append(`<li><span>X</span> ${toDoText}</li>`);
+    $('ul').append(`<li><span class="remove icon"><i class="fa fa-trash-alt"></i></span><span class="edit icon"><i class="fa fa-pencil-alt"></i></span><span class="todo" contenteditable="false"> ${toDoText}<span></li>`);
     // Empty input text field
     $(this).val('');
   }
